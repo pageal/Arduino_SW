c6c952b9a64b2c27b833c2c695faf06f16983fa6
Pavel Gesyuk <pageal@gmail.com>
2015-05-22 22:44:15 +0300

    sender test is added

---
+++ vw/sender_01/sender_01.ino
@@ -0,0 +1,42 @@
+#include <VirtualWire.h>
+
+
+const int led_pin = 13;
+const int transmit_pin = 8;
+const int receive_pin = 7;
+const int transmit_en_pin = 3;
+
+void setup()
+{
+  // Initialise the IO and ISR
+  vw_set_tx_pin(transmit_pin);
+  vw_set_rx_pin(receive_pin);
+  vw_set_ptt_pin(transmit_en_pin);
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);	 // Bits per sec
+}
+
+byte count = 1;
+
+void loop()
+{
+  String msg_prefix = "mp_v01_d01_";
+  String msg = ""; 
+  static bool on_off = false;
+  
+  on_off = !on_off;
+  if(on_off == true)
+  {
+    msg = msg_prefix + "motion";
+  }else
+  {
+    msg = msg_prefix + "no_motion";
+  }
+  
+  digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
+  vw_send((uint8_t *)msg.c_str(), msg.length());
+  vw_wait_tx(); // Wait until the whole message is gone
+  digitalWrite(led_pin, LOW);
+  delay(1000);
+  count = count + 1;
+}
